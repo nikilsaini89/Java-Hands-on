66a138646c11aa5cc608d9899f8fa8c2518b699e
Nikil Saini <nikil.saini@docquity.com>
2025-04-09 09:04:12 +0530

    Encapsulation

--- SimpleTheory.java
+++ SimpleTheory.java
@@ -2,5 +2,10 @@ public class SimpleTheory {
     /*
      * 1. JVM creates objects in Java if we provide it with the bytecode.
      * 2. Every Object consumes some space.
+     * 
+     * ah_Strings includes String, String Buffer, String Builder.
+     * ai_StaticKeyword includes static variables, static methods and static block.
+     * aj_Encapsulation includes Data specifiers, Getter Setters and this Keyword.
+     * 
      */
 }
