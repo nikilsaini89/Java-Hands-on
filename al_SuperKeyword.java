7dc3ffc819aea8436cd1d98180394fbb319355b2
nikilsaini74 <nikilsaini74@gmail.com>
2025-04-09 22:54:52 +0530

    Show method use

--- al_SuperKeyword.java
+++ al_SuperKeyword.java
@@ -8,11 +8,18 @@ class Parent{
         super(); 
         System.out.println("Parent Constructor with int called");
     }
+    public void show(){
+        System.out.println("Inside Parent Show");
+    }
+    public void show(int a){
+        System.out.println("Inside Parent Show with int " + a);
+    }
 }
 class Child extends Parent{
     // This class only extends Parent class and not Object class, so there is no multiple inheritance even now
     public Child(){
-        super();
+        super.show(); // Calls the Parent show method
+        // super.show(5); // Calls the Parent show method with int
         System.out.println("Child Constructor Called");
     }
     public Child(int a){
@@ -32,11 +39,11 @@ public class al_SuperKeyword {
          * 5. We can not use super keyword to access the static and Private members of parent class.
          */
         /**
-         * 
+         * This keyword is used to refer the current class object.
          */
 
         Child c = new Child(); // Calls the Parent constructor first and then Child constructor
         System.out.println();
-        Child c1 = new Child(5); // Calls the Parent constructor with int and then Child constructor with int     
+        // Child c1 = new Child(5); // Calls the Parent constructor with int and then Child constructor with int     
     }
 }
