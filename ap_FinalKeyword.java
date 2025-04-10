8f8ac1533df41a0ee9e48891269515785b763cd4
nikilsaini74 <nikilsaini74@gmail.com>
2025-04-10 08:20:05 +0530

    final

---
+++ ap_FinalKeyword.java
@@ -0,0 +1,11 @@
+public class ap_FinalKeyword {
+    public static void main(String[] args) {
+        /*
+         * Final Keyword
+         * 
+         * i) Class - If a class is final, then no other class can inherit it.
+         * ii) Method - If a method is final, then no inherited class can override this method.
+         * iii) varibale - If a varible is final, then its value can not be changed.
+         */
+    }
+}
