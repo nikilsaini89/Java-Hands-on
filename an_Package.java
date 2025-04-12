998114a60ea7614c12ddd836b295300a9b80b9d0
Nikil Saini <nikil.saini@docquity.com>
2025-04-12 10:44:12 +0530

    Bahar se htado

--- an_Package.java
+++
@@ -1,9 +0,0 @@
-public class an_Package {
-    /*
-     * A Package is a group of classes and/or Packages.
-     * By default every java file imports, java.lang.* -> here * imports all the files of that package. (System, String etc are from this 
-     * package)
-     * ArrayList is from java.util package.
-     * Generally packages are named by reversing the domain name to make it unique because package can be given to others to use itS
-     */
-}
