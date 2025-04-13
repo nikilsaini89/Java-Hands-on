7f1d24c93100835b6140ad2ed9091265e542e8e0
nikilsaini74 <nikilsaini74@gmail.com>
2025-04-13 12:03:30 +0530

    Inner  Class

--- Advance Java/ab_InnerClass.java
+++ Advance Java/ab_InnerClass.java
@@ -1,4 +1,25 @@
-
+class Demo{
+    int age;
+    public void show(){
+        System.out.println("In show");
+    }
+    class Demo2{
+        public void config(){
+            System.out.println("in config");
+        } 
+    }
+}
 public class ab_InnerClass {
-    
+    public static void main(String[] args) {
+        Demo d = new Demo();
+        d.show();
+
+        /*
+         * This is how we can create objecr of inner class if the inner class if not a static class, then we can do - 
+         * Demo.Demo2 d2 = new Demo.Demo2();
+         */
+        Demo.Demo2 d2 = d.new Demo2();
+        d2.config();
+        
+    }
 }
