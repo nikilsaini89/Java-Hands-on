dde75de4b0f4066794c1c05409de22db991b3c42
nikilsaini74 <nikilsaini74@gmail.com>
2025-04-13 12:23:33 +0530

    Ann Inner Class and Abs Ann inner Class

---
+++ Advance Java/ac_AnonymousInnerClass.java
@@ -0,0 +1,23 @@
+class Demo{
+    public void show(){
+        System.out.println("In Demo Show");
+    }
+}
+
+public class ac_AnonymousInnerClass {
+    public static void main(String[] args) {
+        Demo d = new Demo(){
+            public void show1(){
+                System.out.println("In new Show");
+            }
+        };
+        /*
+         * It will first search for show method in the new implementation, if found, it gets it from there else 
+         * it will go in the Demo class and take the implementation from there.
+         * 
+         * It works similar if the Demo class is abstract. Point to note here is that the object is created of the
+         * Inner class always and the Inner class sort of extends the above class. 
+         */
+        d.show();
+    }
+}
